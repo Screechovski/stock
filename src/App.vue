5f4bf0c7071a2847ef17c340ac221e7fd5cf9d12
Yaroslav <webdev1232@gmail.com>
2021-06-16 17:27:04 +0300

    ending project

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    <h2 class="title">My Stock App</h2>
+    <StocksWrapper />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import StocksWrapper from './components/StocksWrapper.vue'
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        StocksWrapper
+    }
 })
 </script>
 
-<style>
+<style lang="scss">
+:root {
+    --textColor: #E4E4E4;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    padding: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #0d0d0d;
+    font-family: 'Baloo Tammudu 2', cursive;
+    height: 100vh;
+    width: 100vw;
+    box-sizing: border-box;
+    overflow: auto;
+    scrollbar-width: none;
+    &::-webkit-scrollbar {
+        display: none;
+    }
+
+}
+.title {
+    font-size: 30px;
+    color: #E4E4E4;
+    text-transform: uppercase;
+    margin-bottom: 1em;
 }
 </style>
---
+++ src/components/StocksWrapper.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="stock">
+        <StockItem
+            v-for="stock, index in stocks" 
+            :key="index"
+            :title="stock.companyName"
+            :abbr="stock.symbol"
+            :price="stock.price"
+            :imageUrl="stock.image"
+        />
+    </div>
+</template>
+
+<script>
+import StockItem from './StockItem.vue';
+import axios from 'axios';
+export default {
+    data(){
+        return {
+            stocks: []
+        }
+    },
+    components: {
+        StockItem
+    },
+    created(){
+        // const apiResponce = await axios.get('https://financialmodelingprep.com/api/v3/profile/AAPL,NVDA,TSLA,AMD,INTC,MDB,SPCE,V,DAL,DOCU,OKTA,AMZN,PINS,TRIP,GDDY,DIS,MCD,NOK,UPWK,IBM,FB,ZM,OZON,NFLX,EA,HLT,H,CCL?apikey=6c9be8fcb7df7894ba5ae48be14935fc');
+        axios.get('https://financialmodelingprep.com/api/v3/profile/AAPL,NVDA,TSLA,AMD,INTC,MDB,SPCE,V,DAL,DOCU,OKTA,AMZN,PINS,TRIP,GDDY,DIS,MCD,NOK,UPWK,IBM,FB,ZM,OZON,NFLX,EA,HLT,H,CCL?apikey=0a63e6339c468c8bb8596525bfb5fbe3')
+            .then(response => {
+                console.log(response.data);
+                return response;
+            })
+            .then(({data})=>this.stocks = data)
+    }
+}
+</script>
+
+<style lang="scss">
+.stock {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 20px;
+    @media screen and (max-width: 991px) {
+        grid-template-columns: repeat(3, 1fr);
+        gap: 15px;
+    }
+}
+</style>
