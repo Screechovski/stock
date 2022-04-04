1b5874eb93929adef3287e99d5af4995562e0ec6
yaroslav dmitriev <webdev1232@gmail.com>
2022-04-04 20:48:16 +0300

    update

--- src/components/StockWrapper.vue
+++ src/components/StockWrapper.vue
@@ -16,6 +16,8 @@ import axios from 'axios';
 import StockItem from '@/components/StockItem.vue';
 import companies from '@/assets/companies';
 
+const apiKey = '1c15180172c4cc4f4ab4e0e5407fec7b'; // '9a19f71b023140c8630b1f4111412cab';
+
 export default {
   data() {
     return {
@@ -28,7 +30,7 @@ export default {
   created() {
     companies.forEach((companyName, index) => {
       setTimeout(() => {
-        axios.get(`https://financialmodelingprep.com/api/v3/profile/${companyName}?apikey=9a19f71b023140c8630b1f4111412cab`)
+        axios.get(`https://financialmodelingprep.com/api/v3/profile/${companyName}?apikey=${apiKey}`)
           .then((response) => {
             console.log(response.data);
             this.stocks = [...this.stocks, ...response.data];
@@ -44,9 +46,9 @@ export default {
 .stock {
     width: 100%;
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(3, 1fr);
     gap: 20px;
-    @media screen and (max-width: 991px) {
+    @media screen and (max-width: 1367px) {
         grid-template-columns: repeat(3, 1fr);
         gap: 15px;
     }
